51485617572b16cafb16cb593ab29104289b24ef
U740034 <erich.weiss@lhind.dlh.de>
2025-02-26 09:34:40 +0100

    removed everything viewController related, because its not necessary

--- ios/Classes/FlutterNativeHtmlToPdfPlugin.swift
+++ ios/Classes/FlutterNativeHtmlToPdfPlugin.swift
@@ -18,33 +18,16 @@ public class FlutterNativeHtmlToPdfPlugin: NSObject, FlutterPlugin {
            let htmlFilePath = args!["htmlFilePath"] as? String
            
            // !!! this is workaround for issue with rendering PDF images on iOS !!!
-           let viewControler = UIApplication.shared.delegate?.window??.rootViewController
-           wkWebView = WKWebView.init(frame: viewControler!.view.bounds)
-           wkWebView.isHidden = true
-           wkWebView.tag = 100
-           viewControler?.view.addSubview(wkWebView)
-           
+           wkWebView = WKWebView.init(frame: CGRect.zero)
            let htmlFileContent = FileHelper.getContent(from: htmlFilePath!) // get html content from file
            wkWebView.loadHTMLString(htmlFileContent, baseURL: Bundle.main.bundleURL) // load html into hidden webview
-           
+
            urlObservation = wkWebView.observe(\.isLoading, changeHandler: { (webView, change) in
                // this is workaround for issue with loading local images
                DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
                    let convertedFileURL = PDFCreator.create(printFormatter: self.wkWebView.viewPrintFormatter())
                    let convertedFilePath = convertedFileURL.absoluteString.replacingOccurrences(of: "file://", with: "") // return generated pdf path
-                   if let viewWithTag = viewControler?.view.viewWithTag(100) {
-                       viewWithTag.removeFromSuperview() // remove hidden webview when pdf is generated
-                       
-                       // clear WKWebView cache
-                       if #available(iOS 9.0, *) {
-                           WKWebsiteDataStore.default().fetchDataRecords(ofTypes: WKWebsiteDataStore.allWebsiteDataTypes()) { records in
-                               records.forEach { record in
-                                   WKWebsiteDataStore.default().removeData(ofTypes: record.dataTypes, for: [record], completionHandler: {})
-                               }
-                           }
-                       }
-                   }
-                   
+
                    // dispose WKWebView
                    self.urlObservation = nil
                    self.wkWebView = nil
